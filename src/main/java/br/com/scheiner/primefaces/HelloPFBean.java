f6a10d1e948f6e03aef6c6ac6e280a2e20ab8e27
lscheiner <leandro.capinan@gmail.com>
2024-02-17 22:56:35 -0300

    first commit

---
+++ src/main/java/br/com/scheiner/primefaces/HelloPFBean.java
@@ -0,0 +1,11 @@
+package br.com.scheiner.primefaces;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.faces.view.ViewScoped;
+
+@Component
+@ViewScoped
+public class HelloPFBean {
+
+}
---
+++ src/main/java/br/com/scheiner/primefaces/PrimefacesApplication.java
@@ -0,0 +1,13 @@
+package br.com.scheiner.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimefacesApplication.class, args);
+	}
+
+}
